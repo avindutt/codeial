6c83b4670deb515ea46df77a5573c4aa7b7e4742
avindutt <avindutt2369@gmail.com>
2022-12-20 15:19:30 +0530

    First commit: create the directory structure and setup express server

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('router loaded');
+
+
+
+module.exports = router; // making the file available for the main index.js file
